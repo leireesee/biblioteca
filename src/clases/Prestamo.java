720914e89094ef740b0c62f21cc17f9fbc28dd62
leireesee <leisancabalei@gmail.com>
2023-01-30 22:15:40 +0100

    definicion atributos, getters/setters, tostring

--- src/clases/Prestamo.java
+++ src/clases/Prestamo.java
@@ -1,5 +1,45 @@
 package clases;
 
+import java.sql.Date;
+
 public class Prestamo {
+	
+	private int idLibro;
+	private int idSocio;
+	private Date fecha;
+	private boolean devuelto;
+	
+		
+	@Override
+	public String toString() {
+		return "idLibro;" + idLibro + ", idSocio;" + idSocio + ", fecha;" + fecha + ", devuelto;" + devuelto;
+	}
+	
+	
+	public int getIdLibro() {
+		return idLibro;
+	}
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+	public int getIdSocio() {
+		return idSocio;
+	}
+	public void setIdSocio(int idSocio) {
+		this.idSocio = idSocio;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public boolean isDevuelto() {
+		return devuelto;
+	}
+	public void setDevuelto(boolean devuelto) {
+		this.devuelto = devuelto;
+	}
+	
 	
 }
