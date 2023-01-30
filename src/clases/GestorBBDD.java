9a9ec89355ee8c295f4d021b2feee48aed1b020f
leireesee <leisancabalei@gmail.com>
2023-01-30 22:20:57 +0100

    definicion de metodos insertarLibro, eliminarLibro, getLibro

--- src/clases/GestorBBDD.java
+++ src/clases/GestorBBDD.java
@@ -1,5 +1,17 @@
 package clases;
 
-public class GestorBBDD {
-
+public class GestorBBDD extends Conector {
+	
+	public void insertarLibro(Libro libro) {
+		
+	}
+	
+	public void eliminarLibro(int id) {
+		
+	}
+	
+	public Libro getLibro(int id) {
+		Libro libro = new Libro();
+		return libro;
+	}
 }
